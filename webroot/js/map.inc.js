ff642fa50d7ac443b4a80d4cb532dc802fa438e7
Grégory Van Ossel <63047472+GregVo23@users.noreply.github.com>
2020-12-18 12:43:02 +0100

    Changement de la map Leaflet js

--- webroot/js/map.inc.js
+++ webroot/js/map.inc.js
@@ -3,9 +3,9 @@ console.log("map");
 window.onload = function(){
    let mymap = L.map('mapid').setView([50.8369069, 4.378142], 4460); 
 
-        let map = L.tileLayer('https://tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey=a0a047fedf024fa4925dfc14dc8fbd53', {
+        let map = L.tileLayer('https://tile.thunderforest.com/mobile-atlas/{z}/{x}/{y}.png?apikey=a0a047fedf024fa4925dfc14dc8fbd53', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
+            maxZoom: 12,
             id: 'mapbox/streets-v11',
             tileSize: 512,
             zoomOffset: -1,
