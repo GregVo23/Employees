94ccd2541630bdff279e96e7087b46a9fa775829
Simon Oldenhove <simonoldenhove@gmail.com>
2020-12-27 17:45:52 +0100

    demands

---
+++ src/Model/Table/DemandsTable.php
@@ -0,0 +1,79 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Demands Model
+ *
+ * @method \App\Model\Entity\Demand newEmptyEntity()
+ * @method \App\Model\Entity\Demand newEntity(array $data, array $options = [])
+ * @method \App\Model\Entity\Demand[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Demand get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Demand findOrCreate($search, ?callable $callback = null, $options = [])
+ * @method \App\Model\Entity\Demand patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Demand[] patchEntities(iterable $entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Demand|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Demand saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Demand[]|\Cake\Datasource\ResultSetInterface|false saveMany(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Demand[]|\Cake\Datasource\ResultSetInterface saveManyOrFail(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Demand[]|\Cake\Datasource\ResultSetInterface|false deleteMany(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Demand[]|\Cake\Datasource\ResultSetInterface deleteManyOrFail(iterable $entities, $options = [])
+ */
+class DemandsTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config): void
+    {
+        parent::initialize($config);
+
+        $this->setTable('demands');
+        $this->setDisplayField('demand_no');
+        $this->setPrimaryKey('demand_no');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->integer('demand_no')
+            ->allowEmptyString('demand_no', null, 'create');
+
+        $validator
+            ->integer('emp_no')
+            ->requirePresence('emp_no', 'create')
+            ->notEmptyString('emp_no');
+
+        $validator
+            ->scalar('type')
+            ->requirePresence('type', 'create')
+            ->notEmptyString('type');
+
+        $validator
+            ->scalar('about')
+            ->maxLength('about', 10)
+            ->requirePresence('about', 'create')
+            ->notEmptyString('about');
+
+        $validator
+            ->scalar('status')
+            ->notEmptyString('status');
+
+        return $validator;
+    }
+}
---
+++ tests/Fixture/DemandsFixture.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * DemandsFixture
+ */
+class DemandsFixture extends TestFixture
+{
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // phpcs:disable
+    public $fields = [
+        'demand_no' => ['type' => 'integer', 'length' => null, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'emp_no' => ['type' => 'integer', 'length' => null, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'type' => ['type' => 'string', 'length' => null, 'null' => false, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'comment' => '', 'precision' => null],
+        'about' => ['type' => 'string', 'length' => 10, 'null' => false, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'comment' => '', 'precision' => null],
+        'status' => ['type' => 'string', 'length' => null, 'null' => false, 'default' => 'pending', 'collate' => 'utf8mb4_general_ci', 'comment' => '', 'precision' => null],
+        '_indexes' => [
+            'emp_no' => ['type' => 'index', 'columns' => ['emp_no'], 'length' => []],
+        ],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['demand_no'], 'length' => []],
+            'foreign_key_emp_no_demands' => ['type' => 'foreign', 'columns' => ['emp_no'], 'references' => ['employees', 'emp_no'], 'update' => 'restrict', 'delete' => 'restrict', 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'utf8mb4_general_ci'
+        ],
+    ];
+    // phpcs:enable
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'demand_no' => 1,
+                'emp_no' => 1,
+                'type' => 'Lorem ipsum dolor sit amet',
+                'about' => 'Lorem ip',
+                'status' => 'Lorem ipsum dolor sit amet',
+            ],
+        ];
+        parent::init();
+    }
+}
